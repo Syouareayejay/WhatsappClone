74238ca05161a5018c3768477fb3380d2ef95ca9
Suraj Singh <jatarogiovani@gmail.com>
2021-10-18 14:37:05 +0530

    Add new chat done

--- whatsapp_clone/src/SidebarChat.js
+++ whatsapp_clone/src/SidebarChat.js
@@ -1,16 +1,25 @@
 import React, {useState,useEffect} from 'react'
 import {Avatar} from '@mui/material'
 import "./css/sidebar.css"
+import db from './firebase';
 
 function SidebarChat({id,name,addnewchat}) {
     const [seed,setSeed] = useState("")
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000))
     }, [])
+    const createChat = () => {
+        let room = prompt('Add new room')
+        if(room){
+            db.collection('rooms').add({
+                name : room
+            })
+        }
+    }
     return (
         addnewchat ? 
         (
-            <div className = "sidebar__chat">
+            <div className = "sidebar__chat" onClick = {createChat}>
                 <h2>Add new chat</h2>
             </div>
         ) : 
